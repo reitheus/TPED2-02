cc58ba593ccae2d106bb2c20f0544b7f03543c18
Henrique441 <123331740+Henrique441@users.noreply.github.com>
2024-09-14 20:42:14 -0300

    Add files via upload

---
+++ include/ordenar.h
@@ -0,0 +1,15 @@
+#ifndef QUICKSORT_H
+#define QUICKSORT_H
+
+#include "item.h"
+
+// Função para trocar dois elementos
+void swap(Item *a, Item *b);
+
+// Função de partição 
+int partition(Item arr[], int esq, int dir, Analise *analise);
+
+// implementaçao algoritmo QuickSort
+void quickSort(Item arr[], int esq, int dir, Analise *analise);
+
+#endif // QUICKSORT_H
